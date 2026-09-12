f21eee3b8e7e5879b6e4a68df13309ff00d21abd
Максим Брагин <maxxtii02@gmail.com>
2026-09-12 16:04:12 +0500

    крестики-нолики финальный вариант

---
+++ gameparts/parts.py
@@ -0,0 +1,42 @@
+# gameparts/parts.py
+
+
+class Board:
+    """Класс, который описывает игровое поле."""
+
+    # Новый атрибут
+    field_size = 3
+
+    def __init__(self):
+        self.board = [
+            [" " for _ in range(self.field_size)] for _ in range(self.field_size)
+        ]
+
+    def make_move(self, row, col, player):
+        self.board[row][col] = player
+
+    def display(self):
+        for row in self.board:
+            print("|".join(row))
+            print("-" * 5)
+
+    def is_board_full(self):
+        for i in range(self.field_size):
+            for j in range(self.field_size):
+                if self.board[i][j] == " ":
+                    return False
+        return True
+    
+    def check_win(self, player):
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+        if (all([self.board[i][i] == player for i in range(self.field_size)]) or
+                all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
+            return True
+        return False
+    
+    # Переопределяем метод __str__
+    def __str__(self):
+        return f"Объект игрового поля размером " f"{self.field_size}x{self.field_size}"
