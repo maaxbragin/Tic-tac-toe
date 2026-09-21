86d51c5b7339e6a760208ef2501d768a6af4174b
Максим Брагин <maxxtii02@gmail.com>
2026-09-21 14:43:36 +0500

    добавлено сохранение результатов в файл

--- game.py
+++ game.py
@@ -2,6 +2,11 @@ from gameparts import Board
 
 from gameparts.exceptions import FieldIndexError, CellOccupiedError
 
+def save_result(result):
+    file = open('results.txt', 'a')
+    file.write(result)
+    file.close()
+
 
 def main():
     game = Board()
@@ -40,10 +45,14 @@ def main():
         print("Ход сделан!")
         game.display()
         if game.check_win(current_player):
-            print(f'Победили {current_player}.')
+            result = f'Победили: {current_player}.'
+            print(result)
+            save_result(result)
             running = False
         elif game.is_board_full():
-            print('Ничья!')
+            result = 'Ничья!'
+            print(result)
+            save_result(result)
             running = False
         current_player = 'O' if current_player == 'X' else 'X'
 
